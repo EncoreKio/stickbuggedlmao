1359843e5034f8b37095ffc965f581bb66c07914
Kio <islemmaki11@gmail.com>
2022-12-19 18:51:00 +0100

    fixed an bug with audio on mobile (i hope it's fixed)

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const button = document.getElementById("button");
 const gif = document.getElementById("gif");
-const title = document.getElementById("title");
 
 button.addEventListener("click",startStick)
 
 function startStick(){
     button.className = "hidden";
     gif.className = "";
-    button.insertAdjacentHTML('afterend', '<audio src="scr/music.mp3" loop preload="auto" autoplay></audio>');
-    title.innerText = "Get stick bugged lmao";
+    button.insertAdjacentHTML('afterend', '<audio src="scr/music.mp3" loop autoplay="autoplay"></audio>');
+    document.title = "Get stick bugged lmao";
 }
